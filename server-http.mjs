f2c742e9671356b3f66fdd93cffd5af4a861674b
Eli_G <eli.grosseau21@gmail.com>
2025-09-21 23:31:57 +1100

    update async/await on server-http.mjs

--- server-http.mjs
+++ server-http.mjs
@@ -4,17 +4,17 @@ import fs from "node:fs/promises";
 const host = "localhost";
 const port = 8000;
 
-function requestListener(_request, response) {
-  fs.readFile("index.html", "utf8")
-    .then((contents) => {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end(contents);
-    })
-    .catch((error) => {
-      console.error(error);
-      response.writeHead(500);
-      response.end( "Error 500: Internal Server Error" );
-    });
+async function requestListener(_request, response) {
+  try {
+    const contents = await fs.readFile("index.html", "utf8");
+    response.setHeader("Content-Type", "text/html");
+    response.writeHead(200);
+    response.end(contents);
+  } catch (error) {
+    console.error(error);
+    response.writeHead(500);
+    response.end("Error 500: Internal Server Error");
+  }
 }
 
 const server = http.createServer(requestListener);
