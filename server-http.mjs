498aa442ff748e0d0752a0a0328118d73340451d
Eli_G <eli.grosseau21@gmail.com>
2025-09-22 15:35:50 +1100

    Partie 1 fini

--- server-http.mjs
+++ server-http.mjs
@@ -4,18 +4,48 @@ import fs from "node:fs/promises";
 const host = "localhost";
 const port = 8000;
 
-async function requestListener(_request, response) {
+async function requestListener(request, response) {
+  response.setHeader("Content-Type", "text/html");
+  const parts = request.url.split("/");
   try {
-    const contents = await fs.readFile("index.html", "utf8");
-    response.setHeader("Content-Type", "text/html");
-    response.writeHead(200);
-    response.end(contents);
+    switch (parts[1]) {
+      case "":
+      case "index.html": {
+        const contents = await fs.readFile("index.html", "utf8");
+        response.writeHead(200);
+        return response.end(contents);
+      }
+      case "random.html": {
+        response.writeHead(200);
+        return response.end(`<html><p>${Math.floor(100 * Math.random())}</p></html>`);
+      }
+      case "random": {
+        const nb = Number.parseInt(parts[2], 10);
+        if (!Number.isNaN(nb) && nb > 0) {
+          let html = "<html>";
+          for (let i = 0; i < nb; i++) {
+            html += `<p>${Math.floor(100 * Math.random())}</p>`;
+          }
+          html += "</html>";
+          response.writeHead(200);
+          return response.end(html);
+        } else {
+          response.writeHead(400);
+          return response.end(`<html><p>400: BAD REQUEST</p></html>`);
+        }
+      }
+      default: {
+        response.writeHead(404);
+        return response.end(`<html><p>404: NOT FOUND</p></html>`);
+      }
+    }
   } catch (error) {
     console.error(error);
     response.writeHead(500);
-    response.end("Error 500: Internal Server Error");
+    return response.end(`<html><p>500: INTERNAL SERVER ERROR</p></html>`);
   }
 }
+
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
